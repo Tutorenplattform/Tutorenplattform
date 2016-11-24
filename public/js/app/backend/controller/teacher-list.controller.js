19a57c2c71d5946c916952b234861609792f25b1
Philip95 <philip.strobach@gmail.com>
2016-11-24 13:53:28 +0100

    create teacher-list.controller.js

---
+++ public/js/app/backend/controller/teacher-list.controller.js
@@ -0,0 +1,22 @@
+(function() {
+
+    angular.module('tp.backend.controller')
+        .controller('TeacherListController', TeacherListController);
+
+    TeacherListController.$inject = ['teachers', '$state'];
+
+    function TutorListController(teachers, $state) {
+        var vm = this;
+
+        vm.teachers = [{id: 1, first_name: 'First', last_name: 'Last'}];
+
+        vm.viewTeacher = viewTeacher;
+
+        function viewTutor(teacher) {
+            $state.go('teacher', {
+                id: teacher.id
+            });
+        }
+    }
+
+})();
