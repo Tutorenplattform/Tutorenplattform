c959b83cf31fb582581fb1f4786693f6b30435d0
Andre Mategka <mategka.andre@gmail.com>
2016-11-28 09:54:56 +0100

    Add permission to tutor registration state

--- public/js/app/app.route.js
+++ public/js/app/app.route.js
@@ -86,6 +86,9 @@
                         controller: 'TutorRegistrationController',
                         controllerAs: 'vm'
                     }
+                },
+                permissions: {
+                    type: 'tutand'
                 }
             });
 
