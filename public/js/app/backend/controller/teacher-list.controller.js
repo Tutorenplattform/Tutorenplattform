ba9ac11444fd6ce031aed04be5c8c538626e1b7a
Andre Mategka <mategka.andre@gmail.com>
2016-11-28 16:09:35 +0100

    Update teacher list to accomodate refresh functionality

--- public/js/app/backend/controller/teacher-list.controller.js
+++ public/js/app/backend/controller/teacher-list.controller.js
@@ -3,19 +3,17 @@
     angular.module('tp.backend.controller')
         .controller('TeacherListController', TeacherListController);
 
-    TeacherListController.$inject = ['teachers', '$state'];
+    TeacherListController.$inject = ['teachers', 'TeacherService'];
 
-    function TeacherListController(teachers, $state) {
+    function TeacherListController(teachers, TeacherService) {
         var vm = this;
+
+        vm.refreshTeachers = refreshTeachers;
 
         vm.teachers = [{id: 1, first_name: 'First', last_name: 'Last'}];
 
-        vm.viewTeacher = viewTeacher;
-
-        function viewTeacher(teacher) {
-            $state.go('teacher', {
-                id: teacher.id
-            });
+        function refreshTeachers() {
+            TeacherService.refresh();
         }
     }
 
