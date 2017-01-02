faea93c6fe2bf443f533021dddc2aaf53bd29a6d
Andre Mategka <mategka.andre@gmail.com>
2017-01-02 16:19:09 +0100

    Rework tutor profile

--- public/js/app/frontend/controller/tutor-profile.controller.js
+++ public/js/app/frontend/controller/tutor-profile.controller.js
@@ -3,45 +3,91 @@
     angular.module('tp.frontend.controller')
         .controller('TutorProfileController', TutorProfileController);
 
-    TutorProfileController.$inject = ['tutor', '$state', 'TutorService'];
+    TutorProfileController.$inject = ['tutor', '$state', 'TutorService', 'Grade', 'Rating', 'Authentication'];
 
-    function TutorProfileController(tutor, $state, TutorService) {
+    function TutorProfileController(tutor, $state, TutorService, Grade, Rating, Authentication) {
         var vm = this;
 
         vm.tutor = {
             vorname: "Hans",
             nachname: "Mustermann",
             klasse: "4AM",
-            id: 1,
-            faecher: ["AM", "D"],
+            pk_tutor_tutand_id: 1,
+            faecher: [{
+                fach: {
+                    pk_fach_id: 1,
+                    name: 'AM'
+                },
+                lehrer: {
+                    pk_lehrer_id: 2,
+                    vorname: 'Reinhard',
+                    nachname: 'Gottweis'
+                },
+                faehigkeiten_anmerkung: 'Gut im Berechnen von quadratischen Funktionen',
+                letzte_zeugnisnote: 2
+            }],
+            klassenvorstand: 'STR',
+            bevorzugte_orte: '2. Stock, in der Ecke',
+            bevorzugte_zeiten: '13:25-14:15 Uhr',
+            telefon_nr: '1234 567 89 00',
+            email_adresse: 'hans@musterma.nn',
             bewertung_gut: 3,
             bewertung_neutral: 1,
-            bewertung_schlecht: 0
+            bewertung_schlecht: 0,
+            bewertung: null
         };
+
+        vm.Grade = Grade;
+        vm.Rating = Rating;
 
         vm.report = report;
         vm.rate = rate;
+        vm.enableRating = enableRating;
         vm.editProfile = editProfile;
+        vm.sendMail = sendMail;
 
-        function report(tutor) {
-            var report = {
-                byUser: 1
-            };
-            TutorService.report(tutor, report);
+        init();
+
+        function init() {
+            vm.mine = Authentication.canManage(vm.tutor);
         }
 
-        function rate(value) {
-            var rating = {
-                rate: value
-            };
-            TutorService.rate(vm.tutor, rating);
+        function report() {
+            $state.go('tutor.report');
+        }
+
+        function rate(rating) {
+            var alreadyRated = (vm.tutor.bewertung === rating.value);
+            if (alreadyRated) {
+                TutorService.rate(vm.tutor, Rating.NONE).then(onTutorRatingRemoved);
+            } else {
+                TutorService.rate(vm.tutor, rating).then(onTutorRatingCast);
+            }
+
+            function onTutorRatingCast() {
+                vm.tutor.bewertung = rating.value;
+                vm.tutor[rating.countKey]++;
+            }
+
+            function onTutorRatingRemoved() {
+                vm.tutor.bewertung = Rating.NONE.value;
+                vm.tutor[rating.countKey]--;
+            }
+        }
+
+        function enableRating(rating) {
+            if (!vm.tutor.bewertung) {
+                return true;
+            }
+            return vm.tutor.bewertung === rating.value;
         }
 
         function editProfile() {
-            var params = {
-                id: vm.tutor.id
-            };
-            $state.go('editProfile', params);
+            $state.go('tutor.edit');
+        }
+
+        function sendMail() {
+            $state.go('tutor.contact');
         }
     }
 
