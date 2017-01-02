11d5c7e85c9ad161a379c0d50c761e71f7181b01
Andre Mategka <mategka.andre@gmail.com>
2017-01-02 16:09:00 +0100

    Rework tutor list

--- public/js/app/frontend/controller/tutor-list.controller.js
+++ public/js/app/frontend/controller/tutor-list.controller.js
@@ -8,14 +8,46 @@
     function TutorListController(tutors, $state) {
         var vm = this;
 
-        vm.tutors = [{id: 1, first_name: 'First', last_name: 'Last', subjects: [{id: 7, name: 'SEW'}], rating: 9}];
+        vm.tutors = [{
+            vorname: "Hans",
+            nachname: "Mustermann",
+            klasse: "4AM",
+            pk_tutor_tutand_id: 1,
+            faecher: [{
+                fach: {
+                    pk_fach_id: 1,
+                    name: 'AM'
+                },
+                lehrer: {
+                    pk_lehrer_id: 2,
+                    vorname: 'Reinhard',
+                    nachname: 'Gottweis'
+                },
+                faehigkeiten_anmerkung: 'Gut im Berechnen von quadratischen Funktionen',
+                letzte_zeugnisnote: 2
+            }],
+            klassenvorstand: 'STR',
+            bevorzugte_orte: '2. Stock, in der Ecke',
+            bevorzugte_zeiten: '13:25-14:15 Uhr',
+            telefon_nr: '1234 567 89 00',
+            email_adresse: 'hans@musterma.nn',
+            bewertung_gut: 3,
+            bewertung_neutral: 1,
+            bewertung_schlecht: 0
+        }];
 
         vm.viewTutor = viewTutor;
+        vm.getSubjects = getSubjects;
 
         function viewTutor(tutor) {
             $state.go('tutor', {
-                id: tutor.id
+                id: tutor.pk_tutor_tutand_id
             });
+        }
+
+        function getSubjects(tutor) {
+            var subjects = _.map(tutor.faecher, 'fach.name');
+            return subjects.join(', ');
         }
     }
 
