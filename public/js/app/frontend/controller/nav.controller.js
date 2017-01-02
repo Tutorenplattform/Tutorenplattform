bd7e8bd186a740e378dd4e81fbe17697b9953f66
Andre Mategka <mategka.andre@gmail.com>
2017-01-02 16:06:53 +0100

    Add frontend navigation

--- public/js/app/frontend/controller/nav.controller.js
+++ public/js/app/frontend/controller/nav.controller.js
@@ -3,15 +3,34 @@
     angular.module('tp.frontend.controller')
         .controller('FrontendNavController', FrontendNavController);
 
-    FrontendNavController.$inject = ['Authentication'];
+    FrontendNavController.$inject = ['Authentication', 'UserType', '$state'];
 
-    function FrontendNavController(Authentication) {
-        var vm = this;
+    function FrontendNavController(Authentication, UserType, $state) {
+        var nav = this;
 
-        vm.logout = logout;
+        nav.logout = logout;
+        nav.isTutor = isTutor;
+        nav.goToProfile = goToProfile;
+
+        init();
+
+        function init() {
+            nav.account = Authentication.getAccountInfo();
+        }
 
         function logout() {
             Authentication.logout();
+        }
+
+        function isTutor() {
+            return nav.account.type === UserType.Tutor;
+        }
+
+        function goToProfile() {
+            var params = {
+                id: nav.account.pk_tutor_tutand_id
+            };
+            $state.go('tutor', params);
         }
     }
 
