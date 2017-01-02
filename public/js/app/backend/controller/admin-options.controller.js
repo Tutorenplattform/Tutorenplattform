22fa56989acf0d57997195a50e23dcf87b399e51
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 00:05:54 +0100

    Add documentation for AdminOptionsController

--- public/js/app/backend/controller/admin-options.controller.js
+++ public/js/app/backend/controller/admin-options.controller.js
@@ -5,6 +5,15 @@
 
     AdminOptionsController.$inject = ['options', 'AdministratorService', '$state'];
 
+    /**
+     * This controller provides all background functionality for the options page on the backend side of the
+     * application.
+     * @param {Object.<string, string>} options The current set of options
+     * @param {AdministratorService} AdministratorService The data service used to interact with the server-side REST
+     * API
+     * @param {$state} $state The service used to transition between states
+     * @constructor
+     */
     function AdminOptionsController(options, AdministratorService, $state) {
         var vm = this;
 
@@ -25,6 +34,9 @@
             vm.fields.email = vm.options.email;
         }
 
+        /**
+         * Saves the changes that were made in the view model and sends an appropriate request to the REST API.
+         */
         function saveChanges() {
             var data = angular.copy(vm.fields);
             if (passwordSet()) {
@@ -33,12 +45,23 @@
             AdministratorService.saveOptions(data);
         }
 
+        /**
+         * Returns whether or not one of the password fields has been filled out. This method is mainly used to
+         * determine whether or not the password fields should be considered required for the view form.
+         * @returns {boolean} true if either password field has been filled out, false otherwise
+         */
         function passwordSet() {
             var passwort1 = vm.fields.passwort;
             var passwort2 = vm.fields.passwort2;
             return passwort1 || passwort2;
         }
 
+        /**
+         * Returns whether or not the changes made in the view model are valid. This mainly involves checking if both of
+         * the passwords are equivalent (if set).
+         * @see passwordSet
+         * @returns {boolean} true if the custom validation ends in success, false otherwise
+         */
         function validChanges() {
             var passwort1 = vm.fields.passwort;
             var passwort2 = vm.fields.passwort2;
@@ -48,6 +71,11 @@
             return true;
         }
 
+        /**
+         * Sends an API request to reset the application. This process involves removing all tutor and tutand accounts
+         * from the server (but not from the Active Directory). Afterwards, the administrator is forwarded to the
+         * backend home page.
+         */
         function reset() {
             AdministratorService.resetApplication().then(forwardToHome);
 
