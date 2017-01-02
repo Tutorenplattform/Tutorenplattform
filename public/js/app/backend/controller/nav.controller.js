4a0e615214690956a29edc4d53f07b5cb7618e34
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 00:06:20 +0100

    Add documentation for BackendNavController

--- public/js/app/backend/controller/nav.controller.js
+++ public/js/app/backend/controller/nav.controller.js
@@ -5,6 +5,12 @@
 
     BackendNavController.$inject = ['Authentication'];
 
+    /**
+     * This controller provides all background functionality for the navigation section on the backend side of the
+     * application.
+     * @param {Authentication} Authentication The service used to interact with the current active user session
+     * @constructor
+     */
     function BackendNavController(Authentication) {
         var vm = this;
 
@@ -16,6 +22,9 @@
             nav.account = Authentication.getAccountInfo();
         }
 
+        /**
+         * Sends an API request to log the administrator out. Afterwards, they will be redirected to the login page.
+         */
         function logout() {
             Authentication.logout();
         }
