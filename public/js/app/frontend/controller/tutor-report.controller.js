606c1fe4514b9ac045030726c7d6ffdb632127a9
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 00:00:47 +0100

    Add documentation for TutorReportController

--- public/js/app/frontend/controller/tutor-report.controller.js
+++ public/js/app/frontend/controller/tutor-report.controller.js
@@ -5,6 +5,14 @@
 
     TutorReportController.$inject = ['tutor', '$state', 'TutorService'];
 
+    /**
+     * This controller provides the background functionality for the tutor report page on the frontend side of the
+     * application.
+     * @param {Object.<string, Object>} tutor The tutor the user wishes to report
+     * @param {$state} $state The service used to transition between states
+     * @param {TutorService} TutorService The data service used to interact with the server-side REST API
+     * @constructor
+     */
     function TutorReportController(tutor, $state, TutorService) {
         var vm = this;
 
@@ -39,11 +47,19 @@
         vm.report = report;
         vm.returnToProfile = returnToProfile;
 
+        /**
+         * Sends an API request to report the current tutor with the attached comment from the view model. Afterwards,
+         * the used will be redirected to the profile of the tutor they just reported.
+         * @see returnToProfile
+         */
         function report() {
             var report = angular.copy(vm.fields);
             TutorService.report(vm.tutor, report).then(returnToProfile);
         }
 
+        /**
+         * Redirects the user back to the current tutor's profile.
+         */
         function returnToProfile() {
             $state.go('tutor');
         }
