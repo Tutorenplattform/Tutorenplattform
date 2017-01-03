8e5f7cb9b592fde063406a913cf1dce56fb75f46
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:24:04 +0100

    Update dummy data in TeacherListController

--- public/js/app/backend/controller/teacher-list.controller.js
+++ public/js/app/backend/controller/teacher-list.controller.js
@@ -10,7 +10,12 @@
 
         vm.refreshTeachers = refreshTeachers;
 
-        vm.teachers = [{id: 1, first_name: 'First', last_name: 'Last'}];
+        vm.teachers = [
+            {pk_lehrer_id: 2, vorname: 'Reinhard', nachname: 'Gottweis'},
+            {pk_lehrer_id: 1, vorname: 'Stephan', nachname: 'Javurek'},
+            {pk_lehrer_id: 3, vorname: 'Kerstin', nachname: 'Stracke-Weiss'},
+            {pk_lehrer_id: 4, vorname: 'Isabella', nachname: 'Fastenbauer'}
+        ];
 
         function refreshTeachers() {
             TeacherService.refresh();
