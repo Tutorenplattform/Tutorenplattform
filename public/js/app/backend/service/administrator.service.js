067e02c49322ea730720efe63fb5d1cb3da2552b
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:20:34 +0100

    Add documentation for Administrator data service

--- public/js/app/backend/service/administrator.service.js
+++ public/js/app/backend/service/administrator.service.js
@@ -5,6 +5,18 @@
 
     AdministratorService.$inject = ['Restangular'];
 
+    /**
+     * A set of administrator options.
+     * @typedef {Object} Options
+     * @property {string} email_adresse The e-mail address of the administrator
+     */
+
+    /**
+     * This service provides methods to interact with the server-side REST API regarding all actions concerning
+     * administrator option actions.
+     * @param {Restangular} Restangular A service providing short-hand methods for Angular's $http service
+     * @constructor
+     */
     function AdministratorService(Restangular) {
         var service = this;
 
@@ -15,14 +27,28 @@
         service.saveOptions = saveOptions;
         service.resetApplication = resetApplication;
 
+        /**
+         * Retrieves the current list of administrator options.
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function getOptions() {
             return adminRest.getList();
         }
 
+        /**
+         * Updates the administrator options using the given options set.
+         * @param {Options} options The new administrator options set
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function saveOptions(options) {
             return adminRest.customPUT(options);
         }
 
+        /**
+         * Resets the application. This process involves removing all tutor and tutand accounts from the server (but not
+         * from the Active Directory).
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function resetApplication() {
             return resetRest.customPOST({});
         }
