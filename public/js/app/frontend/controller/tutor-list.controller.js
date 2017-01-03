a164f172e7ef8538a7b4e50105d7560293d5b1e5
Philip95 <philip.strobach@gmail.com>
2017-01-03 17:38:26 +0100

    add the filter function

--- public/js/app/frontend/controller/tutor-list.controller.js
+++ public/js/app/frontend/controller/tutor-list.controller.js
@@ -12,11 +12,42 @@
 
         vm.viewTutor = viewTutor;
 
+
+
+        vm.submit = function () {
+            //Hol alle Tutoren aus der Datenbank
+            var  tutoren = TutorService.all();
+
+            var results = [];
+
+            // Es werden Variablen angelegt, mit denen man im JSON Object suchen kann
+            var searchField_name = "name",
+                searchField_class = "klasse",
+                searchField_subject = "fach";
+
+            //Holt die Values aus den Input Feldern nach denen gesucht werden soll
+            var searchname = $scope.lastname,
+                searchclass = $scope.class,
+                searchsubject = $scope.subject;
+
+            for (var i=0 ; i < tutoren.length ; i++) {
+                if (
+                    tutoren[i][searchField_name] == searchname ||
+                    tutoren[i][searchField_class] == searchclass ||
+                    tutoren[i][searchField_subject] == searchsubject
+                ) {
+                    results.push(tutoren[i]);
+                    console.log(results);
+                }
+            }
+        };
+
+
+
         function viewTutor(tutor) {
             $state.go('tutor', {
                 id: tutor.id
             });
         }
     }
-
 })();
