dffb6207e034d9f9dd3b044d0e2a1798968c66fa
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:21:27 +0100

    Add documentation for Subject data service

--- public/js/app/backend/service/subject.service.js
+++ public/js/app/backend/service/subject.service.js
@@ -5,6 +5,19 @@
 
     SubjectService.$inject = ['Restangular'];
 
+    /**
+     * A school subject.
+     * @typedef {Object} Subject
+     * @property {number} pk_fach_id The unique identifier of this subject
+     * @property {string} name The name of this subject
+     */
+
+    /**
+     * This service provides methods to interact with the server-side REST API regarding all actions concerning
+     * school subjects.
+     * @param {Restangular} Restangular A service providing short-hand methods for Angular's $http service
+     * @constructor
+     */
     function SubjectService(Restangular) {
         var service = this;
 
@@ -14,14 +27,28 @@
         service.create = create;
         service.destroy = destroy;
 
+        /**
+         * Retrieves the current list of subjects.
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function all() {
             return subjectRest.getList();
         }
 
+        /**
+         * Creates a new subject using the given object.
+         * @param {Subject} subject The object containing the necessary data for creating a new subject
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function create(subject) {
             return subjectRest.post(subject);
         }
 
+        /**
+         * Deletes the entry of the given subject.
+         * @param {Subject} subject The subject to delete
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function destroy(subject) {
             return subject.remove();
         }
