42eebfcc001d6493beb7077bb538fa5323d979cd
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:22:34 +0100

    Add documentation for fullName filter

--- public/js/app/shared/filter/full-name.filter.js
+++ public/js/app/shared/filter/full-name.filter.js
@@ -3,9 +3,19 @@
     angular.module('tp.shared.filter')
         .filter('fullName', fullNameWrapper);
 
+    /**
+     * Returns the fullName filter function.
+     * @see fullNameWrapper#fullName
+     * @returns {function} The fullName filter
+     */
     function fullNameWrapper() {
         return fullName;
 
+        /**
+         * Combines the first and last name of the given person.
+         * @param {Tutor|Tutand|Teacher|ReportedTutor} person The person whose name to construct
+         * @returns {string} The combined first and last name of the given person, separated by a space
+         */
         function fullName(person) {
             return person.vorname + ' ' + person.nachname;
         }
