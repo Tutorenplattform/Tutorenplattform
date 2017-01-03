ad51351265369646da745c2d9d6ed69b973cb009
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:23:18 +0100

    Restructure Rating constant

--- public/js/app/shared/constant/rating.constant.js
+++ public/js/app/shared/constant/rating.constant.js
@@ -1,18 +1,18 @@
 (function() {
 
-    var Rating = {
-        GREAT: new _Rating('GUT', 'bewertung_gut'),
-        OKAY: new _Rating('NEUTRAL', 'bewertung_neutral'),
-        BAD: new _Rating('SCHLECHT', 'bewertung_schlecht'),
-        NONE: new _Rating(null)
+    var Ratings = {
+        GREAT: new Rating('GUT', 'bewertung_gut'),
+        OKAY: new Rating('NEUTRAL', 'bewertung_neutral'),
+        BAD: new Rating('SCHLECHT', 'bewertung_schlecht'),
+        NONE: new Rating(null)
     };
 
-    function _Rating(value, countKey) {
+    function Rating(value, countKey) {
         this.value = value;
         this.countKey = countKey;
     }
 
     angular.module('tp.shared.constant')
-        .constant('Rating', Rating);
+        .constant('Rating', Ratings);
 
 })();
