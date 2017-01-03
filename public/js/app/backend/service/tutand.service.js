c962209283fd1ea25d6e1ef72e8892ab78e94272
Andre Mategka <mategka.andre@gmail.com>
2017-01-03 21:19:45 +0100

    Add documentation for Tutand data service

--- public/js/app/backend/service/tutand.service.js
+++ public/js/app/backend/service/tutand.service.js
@@ -5,6 +5,19 @@
 
     TutandService.$inject = ['Restangular'];
 
+    /**
+     * A normal user that can be taught by tutors.
+     * @typedef {Object} Tutand
+     * @property {number} pk_tutor_tutand_id The unique identifier of this tutand
+     * @property {string} vorname This tutand's first name
+     * @property {string} nachname This tutand's last name
+     */
+
+    /**
+     * This service provides methods to interact with the server-side REST API regarding all actions concerning tutands.
+     * @param {Restangular} Restangular A service providing short-hand methods for Angular's $http service
+     * @constructor
+     */
     function TutandService(Restangular) {
         var service = this;
 
@@ -13,10 +26,19 @@
         service.all = all;
         service.destroy = destroy;
 
+        /**
+         * Retrieves the list of tutands.
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function all() {
             return tutandRest.getList();
         }
 
+        /**
+         * Deletes the entry of the given tutand.
+         * @param {Tutand} tutand The tutand to delete
+         * @returns {Promise} A promise that finishes upon receiving the HTTP response
+         */
         function destroy(tutand) {
             return tutand.remove();
         }
